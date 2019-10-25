5b3275f470868acb4b90795f6225698b77df008f
Ammon <rdlulu@gmail.com>
2019-10-25 00:33:57 -0700

    consolidated to one loop only

--- Cake.java
+++ Cake.java
@@ -8,58 +8,41 @@ public class Cake {
         //draw pan
         for(int i = 0; i < this.cake.length; i++) {
             for(int j = 0; j < this.cake[0].length; j++) {
+                //draw pan
                 int val = 2 * (i - 30) * (i - 30) + (j - 50) * (j - 50) / 4;
                 if(val <= 280 && val > 260) {
                     this.cake[i][j] = 'X';
-                    //this.drawCandle(i, j);
                 }
                 else {
                     this.cake[i][j] = ' ';
                 }
-            }
-        }
-        //draw cake surface
-        for(int i = 0; i < this.cake.length; i++) {
-            for(int j = 0; j < this.cake[0].length; j++) {
+                //draw cake surface
                 if(2 *(i - 20) * (i - 20) + (j - 50) * (j - 50) / 4 <= 100) {
                     this.cake[i][j] = 'X';
-                    //this.drawCandle(i, j);
                 }
-
-            }
-        }
-        //draw cake bottom and edges
-        for(int i = 0; i < this.cake.length; i++) {
-            for(int j = 0; j < this.cake[0].length; j++) {
-                if(i < 30 && j > 30 && j < 70 && this.cake[i][j] == 'X' && this.cake[i + 1][j] == ' ') {
-                    this.cake[i + 9][j] = 'X';
-                }
-                else if(i > 20 && i < 31) {
-                    this.cake[i][30] = 'X';
-                    this.cake[i][70] = 'X';
-                }
-            }
-        }
-        //draw random candles
-        for(int i = 0; i < this.cake.length; i++) {
-            for(int j = 0; j < this.cake[0].length; j++) {
+                //draw random candles
                 if(2 *(i - 20) * (i - 20) + (j - 50) * (j - 50) / 4 <= 60) {
-                    this.drawCandle(i, j);
+                    Random r = new Random();
+                    if(r.nextInt(100) == 25) {
+                        int k = 0;
+                        while(k++ <= 10) {
+                            this.cake[i - k][j] = this.cake[i - k][j] == 'X' ? ' ' : 'X';
+                        }
+                    }
+                }
+
+                //draw cake bottom and edges
+                if(i > 10 && i < 40 && j > 30 && j < 70 && this.cake[i - 9][j] == 'X' && this.cake[i - 8][j] == ' ') {
+                    this.cake[i][j] = 'X';
+                }
+                else if(i > 20 && i < 31 && (j == 30 || j == 70)) {
+                    this.cake[i][j] = 'X';
+                    this.cake[i][j] = 'X';
                 }
             }
         }
     }
 
-    private void drawCandle(int i, int j) {
-        Random r = new Random();
-        if(r.nextInt(100) == 25) {
-            int k = 0;
-            while(k++ <= 10) {
-                this.cake[i - k][j] = this.cake[i - k][j] == 'X' ? ' ' : 'X';
-            }
-        }
-
-    }
     public static void main(String[] args) {
         Cake c = new Cake();
         for(int i = 0; i < c.cake.length; i++) {
